865828973751107d93c9feee80f9ae1eff580b66
Adnan Ali <adnan.ahmed.9620@gmail.com>
2021-12-15 20:53:14 +0500

    Assignment incomplete

---
+++ app.js
@@ -0,0 +1,38 @@
+// var firstName = prompt("Enter Your First Name");
+
+// var lastName = prompt("Enter Your Last Name");
+
+// var fullName = firstName + lastName;
+
+
+// alert(fullName)
+
+// var favPhone = prompt("Enter your Fav mobile phone model");
+
+// var favPhonelen = favPhone.length;
+
+
+// document.write("" + favPhone + " < br > " + favPhonelen);
+
+// var str = "Pakistani";
+
+// var result = str.indexOf("n");
+
+
+// document.write("String: " + str + "<br>" +
+
+//       "Index of 'n': " + result);
+
+// var str = "Hello World";
+
+// var result = str.lastIndexOf("l");
+
+// document.write("String: " + str + "<br>" + "Index of 'n': " + result);
+
+// var character = "Pakistani";
+
+// var result = str.indexOf("n");
+
+
+// document.write("String: " + str + "<br>" +
+//     "Index of 'n': " + result);
